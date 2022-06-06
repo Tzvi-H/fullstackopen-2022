c78ee97e7ac360a2623e7b055e2fd1003a8159b3
Tzvi-H <16guitar@gmail.com>
2022-06-06 15:43:33 -0600

    set up exercise 2.1

---
+++ part2/course-info/src/App.js
@@ -0,0 +1,55 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}
+  </p>
+);
+
+export default App;
