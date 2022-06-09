43ecf929eaa0eaf70e598f3dec1606a1e5c2cb0e
Tzvi-H <16guitar@gmail.com>
2022-06-09 14:35:57 -0600

    completed 3b up to The whole app to internet

--- part2/follow-along/src/services/notes.js
+++ part2/follow-along/src/services/notes.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const baseUrl = "http://localhost:3001/api/notes";
-const baseUrl = "https://limitless-reef-46323.herokuapp.com/api/notes";
+// const baseUrl = "https://limitless-reef-46323.herokuapp.com/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
