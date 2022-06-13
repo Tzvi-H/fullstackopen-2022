70c85dbeb4da9efa947d48602597dcabbfca7047
Tzvi-H <16guitar@gmail.com>
2022-06-13 17:46:55 -0600

    completed exercise 4.10

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -30,6 +30,29 @@ describe("when there is initially some blogs saved", () => {
   });
 });
 
+describe("addition of a new blog", () => {
+  test("succeeds with valid data", async () => {
+    const newBlog = {
+      title: "test title",
+      author: "test author",
+      url: "https://testurl.com/",
+      likes: 10,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const contents = blogsAtEnd.map((n) => n.title);
+    expect(contents).toContain("test title");
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
