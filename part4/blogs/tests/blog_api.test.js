b3138cabf2e8e3c45566729323d2e483da5ffdb0
Tzvi-H <16guitar@gmail.com>
2022-06-13 17:37:38 -0600

    completed exercise 4.8

---
+++ part4/blogs/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const helper = require("./test_helper");
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+describe("when there is initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
