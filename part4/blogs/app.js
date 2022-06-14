47130675d7ff1ba38cc07b0d9bd4cb74cc88cd91
Tzvi-H <16guitar@gmail.com>
2022-06-14 12:53:59 -0600

    completed exercise 4.18

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -8,6 +8,7 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
