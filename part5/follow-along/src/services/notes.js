a50ec1f1b47c5e2c676f9d4bb733557d5d71cc9c
Tzvi-H <16guitar@gmail.com>
2022-06-15 16:23:33 -0600

    completed 5.a

---
+++ part5/follow-along/src/services/notes.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+const baseUrl = "/api/notes";
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const request = axios.post(baseUrl, newObject, config);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+const noteService = {
+  getAll,
+  create,
+  update,
+  setToken,
+};
+
+export default noteService;
