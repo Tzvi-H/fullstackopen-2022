adb95fc2faa54447b2b8fd8c75f479c277c9fd67
Tzvi-H <16guitar@gmail.com>
2022-06-19 11:49:10 -0600

    completed exercise 5.9

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,9 +1,19 @@
 import Blog from "./Blog";
 
 const Blogs = ({ blogs, handleUpdateBlog }) => {
+  const blogsToShow = blogs.sort((b1, b2) => {
+    if (b1.likes > b2.likes) {
+      return -1;
+    } else if (b2.likes > b1.likes) {
+      return 1;
+    } else {
+      return 1;
+    }
+  });
+
   return (
     <div>
-      {blogs.map((blog) => (
+      {blogsToShow.map((blog) => (
         <Blog key={blog.id} blog={blog} handleUpdateBlog={handleUpdateBlog} />
       ))}
     </div>
