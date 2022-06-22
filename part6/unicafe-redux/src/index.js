ee762f98e092a0b9089a5877eee8ffef18cb82c6
Tzvi-H <16guitar@gmail.com>
2022-06-22 15:17:13 -0600

    completed exercise 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const dispatchTypeOf = (type) => () => store.dispatch({ type });
+
+  const { good, ok, bad } = store.getState();
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={dispatchTypeOf("GOOD")}>good</button>
+      <button onClick={dispatchTypeOf("OK")}>ok</button>
+      <button onClick={dispatchTypeOf("BAD")}>bad</button>
+      <button onClick={dispatchTypeOf("ZERO")}>reset stats</button>
+      <div>good {good}</div>
+      <div>ok {ok}</div>
+      <div>bad {bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
