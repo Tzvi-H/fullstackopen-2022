502edb2c69a26408a0e5f9033b441a65ab67e09e
Tzvi-H <16guitar@gmail.com>
2022-06-22 16:09:32 -0600

    completed exercise 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,34 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { voteFor } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const anecdotesToShow = anecdotes.sort((a, b) => {
-    if (a.votes > b.votes) {
-      return -1;
-    } else if (b.votes > a.votes) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotesToShow.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(voteFor(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      ))}
-
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { voteFor } from "../reducers/anecdoteReducer";
+
+const sortByVotes = (anecdotes) => {
+  return anecdotes.sort((a, b) => {
+    if (a.votes > b.votes) {
+      return -1;
+    } else if (b.votes > a.votes) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => sortByVotes(state));
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(voteFor(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
