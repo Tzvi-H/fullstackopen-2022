8c80dabd9375d51db8926404cd95f0bad7721e4a
Tzvi-H <16guitar@gmail.com>
2022-06-23 09:29:05 -0600

    completed exercise 6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import anecdoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteFilter from "./components/AnecdoteFilter";
 import Notification from "./components/Notification";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,28 +1,9 @@
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
+    case "SET_ANECTDOTES":
+      return action.data;
     case "CREATE":
-      return [...state, asObject(action.data)];
+      return [...state, action.data];
     case "VOTE":
       const anecdote = state.find((a) => a.id === action.data);
       const changedAnecdote = { ...anecdote, votes: anecdote.votes + 1 };
@@ -43,6 +24,13 @@ export const voteFor = (id) => {
   return {
     type: "VOTE",
     data: id,
+  };
+};
+
+export const setAnecdotes = (anecdotes) => {
+  return {
+    type: "SET_ANECTDOTES",
+    data: anecdotes,
   };
 };
 
