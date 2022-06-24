ab8660246bf8e62dfe9e2bd62067e887c0a082c0
Tzvi-H <16guitar@gmail.com>
2022-06-24 10:39:08 -0600

    completed exercise 7.1

---
+++ part7/routed-anecdotes/src/index.js
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
