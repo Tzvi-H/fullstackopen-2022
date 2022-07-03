a121ae93a3b186bdd69ed93af7f739b6762978e6
Tzvi-H <16guitar@gmail.com>
2022-07-03 16:23:42 -0600

    completed exercise 8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,10 +97,18 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -109,6 +117,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((b) => b.author === root.name).length,
   },
 };
 
