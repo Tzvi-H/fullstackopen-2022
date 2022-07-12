8f36026b18dd5a157186704b3e46d1f65ce74591
Tzvi-H <16guitar@gmail.com>
2022-07-12 16:45:21 -0600

    completed exercise 10.3

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
 
-export default function App() {
-  console.log("line 5, app.js");
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
