cbddc503e19545c824c905f52d447ddf0253e4fe
Tzvi-H <16guitar@gmail.com>
2022-07-13 14:59:53 -0600

    completed exercise 10.4

--- part10/rate-repository-app/src/components/Main.jsx
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -1,10 +1,9 @@
-import Constants from "expo-constants";
 import { View, StyleSheet } from "react-native";
 import RepositoryList from "./RepositoryList";
+import AppBar from "./AppBar";
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -13,13 +12,13 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );
 };
 
 export default Main;
-
 /*
 Alert 
 Text 
