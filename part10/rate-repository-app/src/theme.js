39eb6926331f56116d7d33e12876614b0f3df1e6
Tzvi-H <16guitar@gmail.com>
2022-07-14 16:42:47 -0600

    completed exercise 10.10

--- part10/rate-repository-app/src/theme.js
+++ part10/rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
@@ -10,7 +12,11 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      ios: "Arial",
+      android: "Roboto",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
