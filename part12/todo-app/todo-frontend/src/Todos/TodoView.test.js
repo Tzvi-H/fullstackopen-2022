f0fe50e6614849cab11a15f81adf8cce7859de4e
Tzvi-H <16guitar@gmail.com>
2022-07-26 16:17:34 -0600

    restarting part 12

--- part12/todo-app/todo-frontend/src/Todos/TodoView.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import TodoView from "./TodoView";
-
-test('<TodoView /> renders an h1 tag with the text "Todos"', () => {
-  const { container } = render(<TodoView />);
-  const h1 = container.querySelector("h1");
-  expect(h1).toHaveTextContent("Todos");
-});
