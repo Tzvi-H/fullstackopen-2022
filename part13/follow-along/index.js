c3aaa5ad0ae6cb8e933677db9a087ea6d328042d
Tzvi-H <16guitar@gmail.com>
2022-07-27 17:20:28 -0600

    completed exercise 13.a

---
+++ part13/follow-along/index.js
@@ -0,0 +1,83 @@
+require("dotenv").config();
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+Note.sync();
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await Note.findAll();
+  console.log(JSON.stringify(notes, null, 2));
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    console.log(note.toJSON());
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/notes", async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
