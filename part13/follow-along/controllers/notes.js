56d466779d72e6a35626e8101657543b51c8a14f
Tzvi-H <16guitar@gmail.com>
2022-07-28 18:54:16 -0600

    completed 13.c upto "Many-to-many relationships"

--- part13/follow-along/controllers/notes.js
+++ part13/follow-along/controllers/notes.js
@@ -1,25 +1,10 @@
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
 const { Note, User } = require("../models");
-const { SECRET } = require("../util/config");
 const { Op } = require("sequelize");
+const { tokenExtractor } = require("../util/middleware");
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
-  next();
-};
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    res.status(401).json({ error: "token missing" });
-  }
   next();
 };
 
--- part13/follow-along/controllers/users.js
+++ part13/follow-along/controllers/users.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
+const { tokenExtractor } = require("../util/middleware");
 
 const { User, Note } = require("../models");
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  if (!user.admin) {
+    return res.status(401).json({ error: "operation not allowed" });
+  }
+  next();
+};
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
@@ -24,6 +33,22 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
   if (user) {
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
     res.json(user);
   } else {
     res.status(404).end();
